26618b67d6356d07799656f5f4ca450798e015c1
Josh Rowe <josh@nocoded.com>
2021-09-25 07:46:15 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Merge PDF Files with PHP and GhostScript
+
+Merges multiple PDF files into a single PDF file using PHP and Ghostscript
+
+## Read our detailed article:
+
+[Merge PDF Files with PHP and GhostScript](https://orangeable.com/php/pdf-merge)
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    $files = [
+        "page-1.pdf",
+        "page-2.pdf",
+        "page-3.pdf"
+    ];
+
+    file_put_contents(getcwd() . "/pdf/merged.pdf", "");
+
+    $command = "gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=" . getcwd() . "\\pdf\\merged.pdf ";
+
+    foreach ($files as $file) {
+        $command .= getcwd() . "\\pdf\\" . $file . " ";
+    }
+
+    $command .= "2&>1";
+
+    echo $command;
+
+    $result = shell_exec($command);
+
+    echo $result;
+?>
